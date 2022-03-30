8d37e5363bf9493242267a6333c7dcd1e69d1dc4
Ronnel Omboy <omboyronnel@gmail.com>
2022-03-30 15:49:21 +0800

    add

--- project/models.py
+++ project/models.py
@@ -20,7 +20,7 @@ class MeetingRooms(models.Model):
 	mid = models.BigAutoField(primary_key = True)
 	meeting_room = models.CharField(max_length=100, blank=True)
 	isAvailable = models.BooleanField(default=True, blank=True)
-	start_date = models.DateField(blank=True)
+	start_date = models.DateField(blank=True, null=True)
 
 class Reservation(models.Model):
 	rid = models.AutoField(primary_key = True)
